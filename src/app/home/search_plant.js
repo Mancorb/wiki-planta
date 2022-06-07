299b467fcceb7b321d18ccb472f55c2441f01c44
César A. Rovira Montero <66651890+CaesarRovira@users.noreply.github.com>
2022-06-06 22:25:38 -0500

    Función búsqueda modificada

--- src/app/home/search_plant.js
+++ src/app/home/search_plant.js
@@ -9,6 +9,28 @@ function searchPlant(){
     "../plant-blackberry/plant-blackberry.page.html"
   ]
   
+  let sTerm = document.search_bar.searchbar.value;
+      sTerm = sTerm.toLowercase();
+
+      if (sTerm == "ajo"){
+        window.location.href = this.plantsUrls[0];
+      } 
+      else if (sTerm == "limón") {
+        window.location.href = this.plantsUrls[1];
+      } 
+      else if (sTerm == "jitomate cherry" || sTerm == "jitomate" || sTerm == "cherry") {
+        window.location.href = this.plantsUrls[2];
+      } 
+      else if (sTerm == "papa") {
+        window.location.href = this.plantsUrls[3];
+      } 
+      else if (sTerm == "zanahoria") {
+        window.location.href = this.plantsUrls[4];
+      } 
+      else if (sTerm == "zarzamora") {
+        window.location.href = this.plantsUrls[5];
+      }
+  /**
     let sTerm = document.search_bar.searchbar.value;
       sTerm = sTerm.toLowercase();
 
@@ -30,4 +52,5 @@ function searchPlant(){
       else if (sTerm == "zarzamora") {
         window.location.assign(this.plantsUrls[5]);
       }
+      */
   }
