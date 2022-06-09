d2d317563679959dab6d00571c5bccf2d15d0c9e
César A. Rovira Montero <66651890+CaesarRovira@users.noreply.github.com>
2022-06-08 21:04:37 -0500

    Código faltante añadido

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,9 +19,25 @@ export class HomePage {
   }
   
   filtrarBusqueda(){
-    this.busqueda = this.busqueda.toLowerCase( )
-    if (this.busqueda=="zarzamora"){
+    this.busqueda = this.busqueda.toLowerCase();
+    
+    if (this.busqueda=="zarzamora" || this.busqueda=="zarza" || this.busqueda=="mora"){
       this.navCtrl.navigateForward(['plant-blackberry']);
+
+    } else if (this.busqueda=="zanahoria"){
+      this.navCtrl.navigateForward(['plant-carrot']);
+
+    } else if (this.busqueda=="jitomate cherry" || this.busqueda=="jitomate" || this.busqueda=="cherry"){
+      this.navCtrl.navigateForward(['plant-cherry-tomato']);
+
+    } else if (this.busqueda=="ajo"){
+      this.navCtrl.navigateForward(['plant-garlic']);
+
+    } else if (this.busqueda=="limón" || this.busqueda=="limon"){
+      this.navCtrl.navigateForward(['plant-lemon']);
+
+    } else if (this.busqueda=="papa" || this.busqueda=="patata"){
+      this.navCtrl.navigateForward(['plant-potato']);
     }
   }
 
