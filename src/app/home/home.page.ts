5c36da55033d9bc276ca0ef9d02f57a8969817d6
Mancorb <andres.m.bosques@gmail.com>
2022-06-08 19:50:13 -0500

    base para busqueda de plantas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,53 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   
-  plantsUrls = [
-    "../plant-garlic/plant-garlic.page.html",
-    "../plant-lemon/plant-lemon.page.html",
-    "../plant-cherry-tomato/plant-cherry-tomato.page.html",
-    "../plant-potato/plant-potato.page.html",
-    "../plant-carrot/plant-carrot.page.html",
-    "../plant-blackberry/plant-blackberry.page.html"
-  ]
+  busqueda = "";
+
+  constructor(
+    public navCtrl: NavController,
+  ){}
 
   _ionChange(event){
     console.log(event.detail.value);
   }
   
-  searchPlant(){
-
-    let plantsUrls = [
-      "../plant-garlic/plant-garlic.page.html",
-      "../plant-lemon/plant-lemon.page.html",
-      "../plant-cherry-tomato/plant-cherry-tomato.page.html",
-      "../plant-potato/plant-potato.page.html",
-      "../plant-carrot/plant-carrot.page.html",
-      "../plant-blackberry/plant-blackberry.page.html"
-    ]
-
-    let sTerm = String((<HTMLInputElement>document.getElementById('searchbar')).value);
-
-      if (sTerm == "ajo"){
-        location.href = plantsUrls[0];
-      } 
-      else if (sTerm == "limón") {
-        location.href = plantsUrls[1];
-      } 
-      else if (sTerm == "jitomate cherry" || sTerm == "jitomate" || sTerm == "cherry") {
-        location.href = plantsUrls[2];
-      } 
-      else if (sTerm == "papa") {
-        location.href = plantsUrls[3];
-      } 
-      else if (sTerm == "zanahoria") {
-        location.href = plantsUrls[4];
-      } 
-      else if (sTerm == "zarzamora") {
-        location.href = plantsUrls[5];
-      }
-    
-
-
+  filtrarBusqueda(){
+    this.busqueda = this.busqueda.toLowerCase( )
+    if (this.busqueda=="zarzamora"){
+      this.navCtrl.navigateForward(['plant-blackberry']);
+    }
   }
 
 }
